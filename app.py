bb77de3a978b2a9cbd45e687ccef440a7b793aee
EberhardFaber <makssup32@gmail.com>
2020-07-19 17:49:05 +0300

    html + app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def get_time():
     return render_template("timer.html")
 
