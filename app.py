cee16d77db1fcc39258ac56684aac51b12b870f3
EberhardFaber <makssup32@gmail.com>
2020-07-19 23:00:26 +0300

    added set page, wip

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def get_time():
-    return render_template("timer.html")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    y = request.form.get("date")
+    # m = request.form.get("month")
+    # a = request.form.get("day")
+    # b = request.form.get("hour")
+    # c = request.form.get("minute")
+    # d = request.form.get("second")
+    return render_template("set.html")
+
+
+@app.route('/start', methods=['GET', 'POST'])
+def start():
+    y = request.form.get("date")
+    print(y)
+    return render_template("start.html")
 
 
 if __name__ == '__main__':
