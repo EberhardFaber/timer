2aa7a914139d93ae0f5ff4ab0814619a1978e36f
EberhardFaber <makssup32@gmail.com>
2020-08-10 23:38:45 +0300

    edited app

--- app.py
+++ app.py
@@ -4,17 +4,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sucka-my-dicka-you-dummy-stinky-ugly-dumbshmucka'
 
 
-@app.route('/set', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def sett():
     return render_template("set.html")
-
-
-@app.route('/start', methods=['GET', 'POST'])
-def start():
-    y = request.form.get("date")
-    ajs = json.dumps(y)
-    print(ajs)
-    return render_template("start.html")
 
 
 if __name__ == '__main__':
